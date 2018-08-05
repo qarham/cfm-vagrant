4c19d3418afa74188d06b70bc3dc1f7b96c493d1
Vivekananda Shenoy <vshenoy@juniper.net>
2018-08-05 15:16:22 -0700

    Update ntp.sh

--- cfm-1x1-vqfx-7srv/scripts/ntp.sh
+++ cfm-1x1-vqfx-7srv/scripts/ntp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 sudo -i
 yum install -y ntp
+systemctl start ntpd
+systemctl enable ntpd
 cat << EOFF > /etc/ntp.conf
 server 10.19.6.55 iburst maxpoll 9
 driftfile /var/lib/ntp/drift
